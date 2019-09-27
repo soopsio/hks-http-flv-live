bb96a35dad247a696ffc55d20f3e39dc48a340bf
soopsio <admin@soops.io>
2019-09-27 12:09:49 +0800

    更换为芒果tv流

--- flvStart.sh
+++ flvStart.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 /livego &
-ffmpeg  -i  rtmp://live.hkstv.hk.lxdns.com/live/hks  -acodec  copy  -vcodec  copy  -f  flv  rtmp://0.0.0.0:1935/hks/http
+ffmpeg  -i  rtmp://58.200.131.2:1935/livetv/hunantv -acodec  copy  -vcodec  copy  -f  flv  rtmp://0.0.0.0:1935/hks/http
 
 exec "$@"
